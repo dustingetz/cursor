3d4c0852f89c361393474adbf72fa64435048bfd
Karl Hardenstine <khardenstine@gmail.com>
2015-12-08 20:51:53 -0500

    Implement IFn.invoke

--- src/karl_cursor/core.clj
+++ src/karl_cursor/core.clj
@@ -1,10 +1,5 @@
 (ns karl-cursor.core)
 
-
-(defprotocol ICursor
-  (refine [this segment] [this segment not-found])
-  )
-
 (defn root-at [segments f] #(update-in % segments f))
 
 (comment
@@ -15,13 +10,13 @@
   clojure.lang.IDeref
   (deref [_] value)
 
-  ICursor
-  (refine [this segment] (.refine this segment nil))
-  (refine [_ segment not-found]
+  clojure.lang.IFn
+  (invoke [this segments] (.invoke this segments nil))
+  (invoke [_ segments not-found]
     (new Cursor
-         (get-in value [segment] not-found)
+         (get-in value segments not-found)
          (fn [f]
-           (swap-fn! (root-at [segment] (fn [v] (f (or v not-found))))))
+           (swap-fn! (root-at segments (fn [v] (f (or v not-found))))))
          ))
 
   clojure.lang.IAtom
@@ -39,17 +34,20 @@
   (def store (atom {:a {:b 1}, :xs [1 2 3]}))
   (def cur (buildCursor store))
   (-> cur (.swap (fn [x] x)))
-  (-> cur (.refine :c {:d 10}) (.refine :d)  (.swap (fn [x] x)))
 
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.deref))
+  (-> cur (.invoke [:c] {:d 10}) (.invoke [:d])  (.swap (fn [x] x)))
+  (-> cur (.invoke [:c] {:d 10}) (.invoke [:d]) (.deref))
+  (-> cur (.invoke [:c] {:d 10}) (.invoke [:d]) (.swap (constantly 11)))
 
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap (constantly 11)))
 
+  (-> cur (.invoke [:c] {:d 10}) (.swap #(merge % {:z 99})))
+  (-> cur (.invoke [:c] {:d 10}) (.swap merge {:z 99}))
+  (-> cur (.invoke [:c] {:d 10}) (.invoke [:d]) (.swap #(+ % 1 2 3)))
+  (-> cur (.invoke [:c] {:d 10}) (.invoke [:d]) (swap! + 1 2 3))
 
-  (-> cur (.refine :c {:d 10}) (.swap #(merge % {:z 99})))
-  (-> cur (.refine :c {:d 10}) (.swap merge {:z 99}))
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap #(+ % 1 2 3)))
-  (-> cur (.refine :c {:d 10}) (.refine :d) (swap! + 1 2 3))
+  @(cur [:a :b :c] {:d 10})
+  @(cur)
+  @cur
 
   @store
   )
