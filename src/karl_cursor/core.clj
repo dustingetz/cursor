8de3762b7fe59b7e914ab152442d5f16a68a96a6
Karl Hardenstine <khardenstine@gmail.com>
2015-12-08 19:34:42 -0500

    Use not-found in swapping

--- src/karl_cursor/core.clj
+++ src/karl_cursor/core.clj
@@ -2,64 +2,49 @@
 
 
 (defprotocol ICursor
-  (refine [this path] [this path not-found])
+  (refine [this segment] [this segment not-found])
   (value [_])
-  (swap [_ f] [_ f x] [_ f x y] [_ f x y args])
+  (swap [_ f])
   )
 
+(defn root-at [segments f] #(update-in % segments f))
+
+(comment
+  (let [f (root-at [:a :b] inc)]
+    (f {:a {:b 0}})))
 
 (deftype Cursor [value, swap-fn!]
   ICursor
-  (refine [this path] (.refine this path nil))
-  (refine [_ path not-found]
+  (refine [this segment] (.refine this segment nil))
+  (refine [_ segment not-found]
     (new Cursor
-         (get-in value [path] not-found)
-         (fn [f & args]
-           (apply swap-fn!
-                  (fn [v & args]
-                    (apply update-in
-                      (or v not-found)
-                      [path] f args
-                      ))
-                  args))
+         (get-in value [segment] not-found)
+         (fn [f]
+           (swap-fn! (root-at [segment] (fn [v] (f (or v not-found))))))
          ))
-  ;swap is being applied too soon (before d is defined)
   (value [_] value)
   (swap [_ f] (swap-fn! f))
-  (swap [_ f x] (swap-fn! f x))
-  (swap [_ f x y] (swap-fn! f x y))
-  (swap [_ f x y args] (apply swap-fn! f x y args))
   )
 
 
-(defn buildCursor [store] (new Cursor @store #(apply swap! store % %&)))
+(defn buildCursor [store] (new Cursor @store #(swap! store %)))
 
 
 (comment
-  (do
-    (def store (atom {:a {:b 1}, :xs [1 2 3]}))
-    (def cur (buildCursor store))
-    #_ (.value cur)
-    #_ (-> cur (.refine :a) (.value))
-    #_ (-> cur (.refine :a) (.swap (constantly 2)))
-    (-> cur (.refine :c {:d 10}) (.refine :d) (.swap (fn [x] x))))
+  (def store (atom {:a {:b 1}, :xs [1 2 3]}))
+  (def cur (buildCursor store))
+  (-> cur (.swap (fn [x] x)))
+  (-> cur (.refine :c {:d 10}) (.refine :d)  (.swap (fn [x] x)))
 
   (-> cur (.refine :c {:d 10}) (.refine :d) (.value))
 
   (-> cur (.refine :c {:d 10}) (.refine :d) (.swap (constantly 11)))
 
 
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap #(merge % {:z 99})))
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap merge {:z 99}))
-  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap #(+ 1 2 3)))
+  (-> cur (.refine :c {:d 10}) (.swap #(merge % {:z 99})))
+  (-> cur (.refine :c {:d 10}) (.swap merge {:z 99}))
+  (-> cur (.refine :c {:d 10}) (.refine :d) (.swap #(+ % 1 2 3)))
   (-> cur (.refine :c {:d 10}) (.refine :d) (.swap + 1 2 3))
+
   @store
-
-
-  (swap! store #(update-in % [:a :b] inc))
-  (swap! store update-in [:a :b] inc)
   )
-
-
-
-
